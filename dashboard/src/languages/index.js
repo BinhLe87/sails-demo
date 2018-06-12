3a9d935de773f3d24870651c9ba01a42a69bd021
IBL <toanhn@cardano-labo.com>
2018-06-12 14:53:27 +0700

    Add index

---
+++ dashboard/src/languages/index.js
@@ -0,0 +1,13 @@
+import lang_cn from './lang_cn';
+import lang_en from './lang_en';
+import lang_fr from './lang_fr';
+import lang_jp from './lang_jp';
+import lang_ru from './lang_ru';
+
+export {
+    lang_cn,
+    lang_en,
+    lang_fr,
+    lang_jp,
+    lang_ru
+};
