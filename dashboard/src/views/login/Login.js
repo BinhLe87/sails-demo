3b7f6a435605eb0c952a300f665a45d32c3310fc
IBL <toanhn@cardano-labo.com>
2018-06-13 17:04:26 +0700

    Add language selection to Login screen

--- dashboard/src/views/login/Login.js
+++ dashboard/src/views/login/Login.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
-import { message, Spin, Card, Form, Icon, Input, Button, Checkbox, Row, Col } from 'antd';
+import { message, Spin, Card, Form, Icon, Input, Button, Checkbox, Select, Row, Col } from 'antd';
 import './styles.scss';
 
 const FormItem = Form.Item;
+const Option = Select.Option;
 
 @inject("stores")
 class Login extends Component {
@@ -36,6 +37,13 @@ class Login extends Component {
 	render() {
 		const { getFieldDecorator } = this.props.form;
 		let { localization } = this.props.stores;
+        const languages = [
+            { title: 'English', lang_id: 'en', flag: 'http://flags.fmcdn.net/data/flags/w1160/gb.png'},
+            { title: 'Japanese', lang_id: 'jp', flag: 'http://flags.fmcdn.net/data/flags/w1160/jp.png'},
+            { title: 'French', lang_id: 'fr', flag: 'http://flags.fmcdn.net/data/flags/w1160/fr.png'},
+            { title: 'Russian', lang_id: 'ru', flag: 'http://flags.fmcdn.net/data/flags/w1160/ru.png'},
+            { title: 'Chinese', lang_id: 'cn', flag: 'http://flags.fmcdn.net/data/flags/w1160/cn.png'},
+        ]
 
 		return (
 			<Card className="login-panel" title={localization.getText('txtLogin')}>
@@ -65,16 +73,22 @@ class Login extends Component {
 							})(<Input prefix={<Icon type="lock"/>} type="password" placeholder={localization.getText('txtInvalidPassword')}/>)
 						}
 						</FormItem>
-						{/* <FormItem>
-							<Checkbox>Remember me</Checkbox>
-							<a className="login-form-forgot" style={{float: 'right'}} href="">Forgot password</a>
-						</FormItem> */}
-							<Button type="primary" htmlType="submit" className="login-form-button">
-								{localization.getText('txtLogin')}
-							</Button>{' '}
-						{/* <Button type="primary" htmlType="submit" className="login-form-button" onClick={() => this.props.stores.network.login()} style={{float: 'right'}}>
-							Log in with Blockpass
-						</Button> */}
+						<Row>
+							<Col className="gutter-box" span={12}>
+								<Button type="primary" htmlType="submit" className="fullwidth login-form-button">
+									{localization.getText('txtLogin')}
+								</Button>
+							</Col>
+							<Col className="gutter-box" span={12}>
+								<Select defaultValue="en" className="fullwidth" onChange={(langID) => localization.changeLanguage(langID)}>
+								{
+									languages.map((item, idx) => {return (
+										<Option key={item.lang_id} value={item.lang_id}><img src={item.flag} style={{width: 16, height: 12}}/> {item.title}</Option>
+									)})
+								}
+								</Select>
+							</Col>
+						</Row>
 					</Form>
 				</Spin>
 			</Card>
